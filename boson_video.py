b7dc1b7da85cdc4f5a29a6626dd4b00694b542eb
Anil Dhurjaty <adhurjaty@novacoast.com>
2020-03-25 15:37:25 -0700

    shows video in opencv

--- boson_video.py
+++ boson_video.py
@@ -1,84 +1,20 @@
 #!/usr/bin/python3
 
-import sys, os
-import gi
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
 
-gi.require_version('Gst', '1.0')
-from gi.repository import Gst, GObject, Gtk
+vid = cv2.VideoCapture(0)
+vid.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'RGGB'))
+vid.set(cv2.CAP_PROP_CONVERT_RGB, False)
 
-class GTK_Main:
-    def __init__(self):
-        window = Gtk.Window(Gtk.WindowType.TOPLEVEL)
-        window.set_title("Webcam-Viewer")
-        window.set_default_size(512, 640)
-        window.connect("destroy", Gtk.main_quit, "WM destroy")
-        vbox = Gtk.VBox()
-        window.add(vbox)
-        self.movie_window = Gtk.DrawingArea()
-        vbox.add(self.movie_window)
-        hbox = Gtk.HBox()
-        vbox.pack_start(hbox, False, False, 0)
-        hbox.set_border_width(10)
-        hbox.pack_start(Gtk.Label(), False, False, 0)
-        self.button = Gtk.Button("Start")
-        self.button.connect("clicked", self.start_stop)
-        hbox.pack_start(self.button, False, False, 0)
-        self.button2 = Gtk.Button("Quit")
-        self.button2.connect("clicked", self.exit)
-        hbox.pack_start(self.button2, False, False, 0)
-        hbox.add(Gtk.Label())
-        window.show_all()
+while vid.isOpened():
+    empty, frame = vid.read()
 
-        # Set up the gstreamer pipeline
-        self.player = Gst.parse_launch ("v4l2src device=/dev/video0 ! video/x-bayer, format=rggb, width=640, height=513 ! bayer2rgb ! nvvidconv ! nv3dsink")
+    cv2.imshow("s", frame)
 
-        print("launched")
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-        bus = self.player.get_bus()
-        bus.add_signal_watch()
-        bus.enable_sync_message_emission()
-
-        print("enabled")
-
-        bus.connect("message", self.on_message)
-        bus.connect("sync-message::element", self.on_sync_message)
-
-        print("connected")
-
-    def start_stop(self, w):
-        if self.button.get_label() == "Start":
-            self.button.set_label("Stop")
-            self.player.set_state(Gst.State.PLAYING)
-        else:
-            self.player.set_state(Gst.State.NULL)
-            self.button.set_label("Start")
-
-    def exit(self, widget, data=None):
-        Gtk.main_quit()
-
-    def on_message(self, bus, message):
-        t = message.type
-        if t == Gst.MessageType.EOS:
-            self.player.set_state(Gst.State.NULL)
-            self.button.set_label("Start")
-        elif t == Gst.MessageType.ERROR:
-            err, debug = message.parse_error()
-            print(f"Error: {err} {debug}")
-            self.player.set_state(Gst.State.NULL)
-            self.button.set_label("Start")
-
-    def on_sync_message(self, bus, message):
-        struct = message.get_structure()
-        if not struct:
-            return
-        message_name = struct.get_name()
-        if message_name == "prepare-xwindow-id":
-            # Assign the viewport
-            imagesink = message.src
-            imagesink.set_property("force-aspect-ratio", True)
-            imagesink.set_xwindow_id(self.movie_window.window.xid)
-
-Gst.init(None)
-GTK_Main()
-GObject.threads_init()
-Gtk.main()
+vid.release()
+print('done')
